d26dedfcc7b4b6d015b28b349e313cefedcd2e9c
Hinnka <bjzhou1990@gmail.com>
2023-08-16 14:47:48 +0800

    fix stupid error

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ apt install -y unzip
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
 wget -O /usr/local/etc/xray/config.json https://github.com/bjzhou/xray-server-setup/raw/main/xray/config.json
 newpass=`openssl rand -base64 16`
-sed -i "s/PASSWORD_PLACEHOLER/$newpass/g" /usr/local/etc/xray/config.json
+sed -i "s/PASSWORD_PLACEHOLDER/$newpass/g" /usr/local/etc/xray/config.json
 wget -O mosdns.zip https://github.com/IrineSistiana/mosdns/releases/download/v5.1.3/mosdns-linux-amd64.zip
 mkdir mosdns_temp
 unzip mosdns.zip -d mosdns_temp
@@ -16,7 +16,7 @@ rm -rf mosdns_temp
 mkdir /etc/mosdns
 wget -O /etc/mosdns/config.yaml https://github.com/bjzhou/xray-server-setup/raw/main/mosdns/config.yaml
 wget -O /etc/mosdns/geoip_cloudflare.txt https://github.com/bjzhou/xray-server-setup/raw/main/mosdns/geoip_cloudflare.txt
-wget -O /etc/mosdns/geosite_netflix https://github.com/bjzhou/xray-server-setup/raw/main/mosdns/geosite_netflix
+wget -O /etc/mosdns/geosite_netflix.txt https://github.com/bjzhou/xray-server-setup/raw/main/mosdns/geosite_netflix.txt
 wget -O /etc/systemd/system/mosdns.service https://github.com/bjzhou/xray-server-setup/raw/main/mosdns/mosdns.service
 systemctl enable mosdns.service
 systemctl start mosdns.service
@@ -29,4 +29,7 @@ sysctl -p
 
 systemctl restart xray
 ip=`curl ifconfig.me`
-echo "server：$ip\nport：25000\nmethod：2022-blake3-aes-128-gcm\npassword: $newpass"
+echo "server：$ip
+port：25000
+method：2022-blake3-aes-128-gcm
+password: $newpass"
