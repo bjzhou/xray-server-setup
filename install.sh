28f777d109f96c0b8e796977018598d357510cf7
Hinnka <bjzhou1990@gmail.com>
2023-08-28 23:08:05 +0800

    network optimize

--- install.sh
+++ install.sh
@@ -22,10 +22,28 @@ systemctl enable mosdns.service
 systemctl start mosdns.service
 rm -rf mosdns.zip
 
-echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_slow_start_after_idle=0" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_notsent_lowat=16384" >> /etc/sysctl.conf
+echo "
+net.core.default_qdisc = fq
+net.ipv4.tcp_congestion_control = bbr
+net.ipv4.tcp_ecn = 2
+fs.file-max = 1000000
+fs.inotify.max_user_instances = 8192
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_fin_timeout = 30
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.ip_local_port_range = 1024 65000
+net.ipv4.tcp_max_syn_backlog = 16384
+net.ipv4.tcp_max_tw_buckets = 6000
+net.ipv4.route.gc_timeout = 100
+net.ipv4.tcp_syn_retries = 1
+net.ipv4.tcp_synack_retries = 1
+net.core.somaxconn = 32768
+net.core.netdev_max_backlog = 32768
+net.ipv4.tcp_timestamps = 0
+net.ipv4.tcp_max_orphans = 32768
+net.ipv4.tcp_slow_start_after_idle = 0
+net.ipv4.tcp_notsent_lowat = 16384
+" >> /etc/sysctl.conf
 sysctl -p
 
 systemctl restart xray
